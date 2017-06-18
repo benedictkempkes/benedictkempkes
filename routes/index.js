b19e0c2433b55b5d3d0b71b1befe102adeea3f74
BenedictKempkes <benedictkempkes@googlemail.com>
2017-06-18 21:43:43 +0200

    Navigation
    Added navigation on home page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Benedict Kempkes' });
 });
 
+/* GET Angular page*/
+router.get('/angular', function(req, res, next) {
+  res.render('angularViews/angular', { title: 'Persönlicher Blog' });
+});
+
+/* GET React page*/
+router.get('/react', function(req, res, next) {
+  res.render('reactViews/react', { title: 'Ziel Verfolgung' });
+});
+
+
 module.exports = router;
